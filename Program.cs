fa3c472a5b068ef8c819ec873313c0425eb61d00
Bárbara <babyr.oliveira@gmail.com>
2020-02-28 21:28:05 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá usuário, me chamo Bárbara");
+        }
+    }
+}
